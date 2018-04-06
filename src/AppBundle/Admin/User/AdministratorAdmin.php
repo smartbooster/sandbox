16e3673d65703e9c5dcb96adea740f828470541b
Mathieu DUCROT <mathieu.ducrot@pia-production.fr>
2018-04-06 11:47:59 +0200

    #1 : Complete administrators CRUD

--- src/AppBundle/Admin/User/AdministratorAdmin.php
+++ src/AppBundle/Admin/User/AdministratorAdmin.php
@@ -5,6 +5,8 @@ namespace AppBundle\Admin\User;
 use AppBundle\Entity\User\Administrator;
 use Smart\SonataBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
 
 /**
  * @author Nicolas Bastien <nicolas.bastien@smartbooster.io>
@@ -40,6 +42,47 @@ class AdministratorAdmin extends AbstractAdmin
             ->addIdentifier('email', null, ['label' => 'form.label_email'])
             ->add('firstName', null, ['label' => 'form.label_first_name'])
             ->add('lastName', null, ['label' => 'form.label_last_name'])
+        ;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function configureShowFields(ShowMapper $show)
+    {
+        $show
+            ->with('fieldset.label_general')
+                ->add('email', null, ['label' => 'form.label_email'])
+                ->add('firstName', null, ['label' => 'form.label_first_name'])
+                ->add('lastName', null, ['label' => 'form.label_last_name'])
+            ->end()
+        ;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function configureFormFields(FormMapper $form)
+    {
+        $form
+            ->with('fieldset.label_general', ['class' => 'col-md-7'])
+                ->add('email', null, ['label' => 'form.label_email'])
+                ->add('firstName', null, ['label' => 'form.label_first_name'])
+                ->add('lastName', null, ['label' => 'form.label_last_name'])
+            ->end()
+            ->with('fieldset.label_password', ['class' => 'col-md-5 pull-right'])
+                ->add(
+                    'plainPassword',
+                    'repeated',
+                    [
+                        'type'               => 'password',
+                        'required'           => $this->isNew(),
+                        'first_options'      => ['label' => 'form.label_password'],
+                        'second_options'     => ['label' => 'form.label_password_confirmation'],
+                        'options' => array('translation_domain' => 'admin'),
+                    ]
+                )
+            ->end()
         ;
     }
 }
